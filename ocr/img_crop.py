3ba301930645bf8ec865713415ed54dbcfee1aa5
mbrito <miguelfbrito11@gmail.com>
2019-07-25 16:33:18 +0100

    Adjust color cut values for bottom bar and add orange support

--- ocr/img_crop.py
+++ ocr/img_crop.py
@@ -29,17 +29,23 @@ for i in range(1, total_raids):
     print("Prev size {} {}".format(h, w))
     color = img[h-1, 0]
 
-    # TODO : adicionar o laranja da poupança de bateria
-    if co.diff_between_rgb_colors(color, [0, 0, 0]) < 5 or co.diff_between_rgb_colors(color, [255, 255, 255]) < 5:
+    is_black = co.diff_between_rgb_colors(color[::-1], [0, 0, 0]) < 10
+    is_white = co.diff_between_rgb_colors(color[::-1], [255, 255, 255]) < 10
+    is_orange = co.diff_between_rgb_colors(color[::-1], [245, 80, 32]) < 10
+
+    cv.imshow("prev image", img)
+    cv.waitKey(0)
+
+    print("Color initial {} {}".format(
+        color, co.diff_between_rgb_colors(color, img[h-2, 0])))
+
+    if is_black or is_white or is_orange:
         pixel_cut = 0
-        while(co.diff_between_rgb_colors(color, img[h-1, 0]) < 3):
+        while(co.diff_between_rgb_colors(color, img[h-1, 0]) < 5):
             pixel_cut += 1
             h -= 1
             print("{} {}".format(
                 h, co.diff_between_rgb_colors(color, img[h, 0])))
-
-        cv.imshow("prev image", img)
-        cv.waitKey(0)
 
         if pixel_cut > 25:
             img = img[0:h, 0:w]
@@ -54,25 +60,25 @@ for i in range(1, total_raids):
     width = 900
     crop_img = img[0:int(h/3*2), 0:w]
 
-for i in range(1, total_raids):
-    img = read_image(i)
+# for i in range(1, total_raids):
+#     img = read_image(i)
 
-    processed_img = crop_resize_img(img)
+#     processed_img = crop_resize_img(img)
 
-    # cv.rectangle(processed_img, (650, 850),
-    #              (650 + 200, 850 + 50), (0, 255, 0), 5)
+#     # cv.rectangle(processed_img, (650, 850),
+#     #              (650 + 200, 850 + 50), (0, 255, 0), 5)
 
-    # cv.rectangle(resize_img, (int(w_r * 0.1), 340),(int(w_r * 0.9), 340 + 120), (0, 0, 255), 5)
+#     # cv.rectangle(resize_img, (int(w_r * 0.1), 340),(int(w_r * 0.9), 340 + 120), (0, 0, 255), 5)
 
-    pokemon_name_img = crop_pokemon_name(processed_img)
+#     pokemon_name_img = crop_pokemon_name(processed_img)
 
-    thresh = get_threshold(pokemon_name_img, 240, True)
+#     thresh = get_threshold(pokemon_name_img, 240, True)
 
-    text = pytesseract.image_to_string(
-        thresh, config='--psm 7 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz')
+#     text = pytesseract.image_to_string(
+#         thresh, config='--psm 7 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz')
 
-    print("Pokemon name: {}".format(text))
+#     print("Pokemon name: {}".format(text))
 
-    cv.imshow('test', thresh)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+#     cv.imshow('test', thresh)
+#     cv.waitKey(0)
+#     cv.destroyAllWindows()
