77cb18f6a4dd2d5239726cba3243ce1e937d196b
Rui Araújo <ruiaraujo012@gmail.com>
2019-07-26 01:00:59 +0100

    :recycle::truck: Organize code and add more images

--- ocr/img_crop.py
+++ ocr/img_crop.py
@@ -1,84 +1,38 @@
 #!/usr/bin/python3
 
 
-import img_process
+import re
 import pytesseract
-from matplotlib import pyplot as plt
 import cv2 as cv
-import math
-import argparse
-from utils.process_img import *
-import color as co
+from utils import process_img as pri
 
-# parser = argparse.ArgumentParser(description='Name of screenshot.')
-# parser.add_argument('--num', type=int, default=1,
-#                     help='an integer for the path screen')
+TOTAL_RAIDS = 54
 
-# args = parser.parse_args()
+for i in range(0, TOTAL_RAIDS):
 
-# img = cv.imread('raids/raid_' + str(args.num) + '.jpg')
+    # FIXME: remover barra superior, falham as imagens 6 e 41.
 
-total_raids = 35
-
-for i in range(1, total_raids):
-    img_path = "raids/raid_" + str(i) + ".jpg"
+    img_path = pri.read_image_pokemon(i)
 
     img = cv.imread(img_path)
 
-    h, w, c = img.shape
-    print("Prev size {} {}".format(h, w))
-    color = img[h-1, 0]
+    new_img = pri.remove_bottom_bar(img)
 
-    is_black = co.diff_between_rgb_colors(color[::-1], [0, 0, 0]) < 10
-    is_white = co.diff_between_rgb_colors(color[::-1], [255, 255, 255]) < 10
-    is_orange = co.diff_between_rgb_colors(color[::-1], [245, 80, 32]) < 10
+    processed_img = pri.crop_resize_img(new_img)
 
-    cv.imshow("prev image", img)
+    h, w, c = processed_img.shape
+
+    pokemon_name_img = pri.crop_pokemon_name(processed_img)
+
+    thresh = pri.get_threshold(pokemon_name_img, 245, True)
+
+    text = pytesseract.image_to_string(
+        thresh, config="--psm 8")
+
+    text = re.sub('[^a-zA-Z]', '', text).lower()
+
+    print('Text found: {}'.format(text))
+
+    cv.imshow("raids/raid_" + str(i) + ".jpg", thresh)
     cv.waitKey(0)
-
-    print("Color initial {} {}".format(
-        color, co.diff_between_rgb_colors(color, img[h-2, 0])))
-
-    if is_black or is_white or is_orange:
-        pixel_cut = 0
-        while(co.diff_between_rgb_colors(color, img[h-1, 0]) < 5):
-            pixel_cut += 1
-            h -= 1
-            print("{} {}".format(
-                h, co.diff_between_rgb_colors(color, img[h, 0])))
-
-        if pixel_cut > 25:
-            img = img[0:h, 0:w]
-            print("New size {} {}".format(h, w))
-            cv.imshow("new image", img)
-            cv.waitKey(0)
-
-        h, w, c = img.shape
-
-        print('Resolution size: {} x {}'.format(h, w))
-
-    width = 900
-    crop_img = img[0:int(h/3*2), 0:w]
-
-# for i in range(1, total_raids):
-#     img = read_image(i)
-
-#     processed_img = crop_resize_img(img)
-
-#     # cv.rectangle(processed_img, (650, 850),
-#     #              (650 + 200, 850 + 50), (0, 255, 0), 5)
-
-#     # cv.rectangle(resize_img, (int(w_r * 0.1), 340),(int(w_r * 0.9), 340 + 120), (0, 0, 255), 5)
-
-#     pokemon_name_img = crop_pokemon_name(processed_img)
-
-#     thresh = get_threshold(pokemon_name_img, 240, True)
-
-#     text = pytesseract.image_to_string(
-#         thresh, config='--psm 7 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz')
-
-#     print("Pokemon name: {}".format(text))
-
-#     cv.imshow('test', thresh)
-#     cv.waitKey(0)
-#     cv.destroyAllWindows()
+    cv.destroyAllWindows()
--- ocr/utils/process_img.py
+++ ocr/utils/process_img.py
@@ -1,16 +1,15 @@
 import cv2 as cv
+import color as co
 
 
-def read_image(path):
-    img_path = "raids/raid_" + str(path) + ".jpg"
+def read_image_pokemon(number):
+    img_path = "raids/pokemon/" + str(number) + ".jpg"
     img = cv.imread(img_path)
     return img
 
 
 def crop_resize_img(img):
     h, w, c = img.shape
-
-    print('Resolution size: {} x {}'.format(h, w))
 
     width = 900
 
@@ -27,7 +26,7 @@ def crop_resize_img(img):
 def crop_pokemon_name(img):
     h, w, c = img.shape
 
-    crop_img = img[340:460, int(w * 0.1):int(w * 0.9)]
+    crop_img = img[355:475, int(w * 0.1):int(w * 0.9)]
 
     return crop_img
 
@@ -45,3 +44,24 @@ def get_threshold(img, thresh_val=240, which=False):
         return thresh
     else:
         return morph_img
+
+
+def remove_bottom_bar(img):
+    h, w, c = img.shape
+
+    color = img[h-1, 0]
+
+    is_black = co.diff_between_rgb_colors(color[::-1], [0, 0, 0]) < 10
+    is_white = co.diff_between_rgb_colors(color[::-1], [255, 255, 255]) < 10
+    is_orange = co.diff_between_rgb_colors(color[::-1], [245, 80, 32]) < 10
+
+    if is_black or is_white or is_orange:
+        pixel_cut = 0
+        while(co.diff_between_rgb_colors(color, img[h-1, 0]) < 5):
+            pixel_cut += 1
+            h -= 1
+
+        if pixel_cut > 25:
+            img = img[0:h, 0:w]
+
+    return img
