9c00f544f0feceb579f2334ed4fbfde9ec0342a8
Rui Araújo <ruiaraujo012@gmail.com>
2019-07-27 00:58:49 +0100

    :sparkles: Pokedex and current boss list added. Search boss names by raid level added

--- ocr/pokemon_name.py
+++ ocr/pokemon_name.py
@@ -6,6 +6,8 @@ import pytesseract
 import cv2 as cv
 from utils import process_img as pri
 from utils import name_ratio as nr
+from utils import get_bosses as gb
+
 
 TOTAL_RAIDS = 54
 
@@ -24,30 +26,33 @@ for i in range(0, TOTAL_RAIDS):
 
     thresh = pri.get_threshold(pokemon_name_img, 245, True)
 
-    text = pytesseract.image_to_string(
+    text_found = pytesseract.image_to_string(
         thresh, config="--psm 8")
 
-    text = re.sub('[^a-zA-Z]', '', text).lower()
+    boss_name_found = re.sub('[^a-zA-Z]', '', text_found)
 
-    names = ['raikou', 'mesprit', 'dialga',
-             'giratina', 'rayquaza', 'raichu', 'scyther', 'togetic', 'mewtwo', 'articuno', 'drifloon',
-             'machamp', 'sneasel', 'tyranitar', 'graudon', 'cresselia', 'metagross', 'dragonite',
-             'latios', 'marowak', 'hitmonlee', 'absol', 'latias']
+    # TODO: Adicionar um metodo que devolva o nivel da raid e alterar em baixo (default 5)
+    # Sem alterar todos vão ser Mewtwo, Dialga e Raikou
+    bosses_id = gb.current_boss_by_level(5)
+
+    bosses = gb.current_boss_names_by_id(bosses_id)
 
     max_ratio = 0
-    pokemon_name = 'not found'
 
-    for name in names:
-        distance = nr.levenshtein_ratio_and_distance(text, name)
-        ratio = nr.levenshtein_ratio_and_distance(text, name, ratio_calc=True)
+    for boss_name in bosses:
+        distance = nr.levenshtein_ratio_and_distance(
+            boss_name_found.lower(), boss_name.lower())
+        ratio = nr.levenshtein_ratio_and_distance(
+            boss_name_found.lower(), boss_name.lower(), ratio_calc=True)
 
         if ratio > max_ratio:
             max_ratio = ratio
-            pokemon_name = name
+            pokemon_name = boss_name
 
     print(' ' * 20)
     print('=' * 20)
-    print('Text found: {}'.format(text))
+    print('Text found: {}'.format(text_found))
+    print('Text found with regex: {}'.format(boss_name_found))
     print('Best match name: {}'.format(pokemon_name))
     print('Best match ratio: {}'.format(max_ratio))
     print('=' * 20)
