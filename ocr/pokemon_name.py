02384d3146c496fe610b347c546960aedead855e
Rui Araújo <ruiaraujo012@gmail.com>
2019-07-26 13:21:34 +0100

    :sparkles: Added pokemon name recognition

--- ocr/pokemon_name.py
+++ ocr/pokemon_name.py
@@ -5,11 +5,12 @@ import re
 import pytesseract
 import cv2 as cv
 from utils import process_img as pri
+from utils import name_ratio as nr
 
 TOTAL_RAIDS = 54
 
 for i in range(0, TOTAL_RAIDS):
-    # FIXME: remover barra superior, falham as imagens 6 e 41.
+    # FIXME: remover barra superior, falham as imagens 3, 6 e 41.
 
     img = pri.read_image_pokemon(i)
 
@@ -28,7 +29,28 @@ for i in range(0, TOTAL_RAIDS):
 
     text = re.sub('[^a-zA-Z]', '', text).lower()
 
+    names = ['raikou', 'mesprit', 'dialga',
+             'giratina', 'rayquaza', 'raichu', 'scyther', 'togetic', 'mewtwo', 'articuno', 'drifloon',
+             'machamp', 'sneasel', 'tyranitar', 'graudon', 'cresselia', 'metagross', 'dragonite',
+             'latios', 'marowak', 'hitmonlee', 'absol', 'latias']
+
+    max_ratio = 0
+    pokemon_name = 'not found'
+
+    for name in names:
+        distance = nr.levenshtein_ratio_and_distance(text, name)
+        ratio = nr.levenshtein_ratio_and_distance(text, name, ratio_calc=True)
+
+        if ratio > max_ratio:
+            max_ratio = ratio
+            pokemon_name = name
+
+    print(' ' * 20)
+    print('=' * 20)
     print('Text found: {}'.format(text))
+    print('Best match name: {}'.format(pokemon_name))
+    print('Best match ratio: {}'.format(max_ratio))
+    print('=' * 20)
 
     cv.imshow("raid_" + str(i) + ".jpg", thresh)
     cv.waitKey(0)
