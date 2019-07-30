396c605f81cd7242b6fe0b52011027d2cd1d5479
Rui Araújo <ruiaraujo012@gmail.com>
2019-07-30 19:07:29 +0100

    :truck: Clean work directory

---
+++ ocr/utils/extractor.py
@@ -0,0 +1,123 @@
+import pytesseract
+import cv2 as cv
+import numpy as np
+from utils import process_img as pi
+from utils import process_text as pt
+
+
+def scan_for_current_time(img):
+    # Verificar 7.8.9 - aparecem a branco
+    h, w, c = img.shape
+    vscale = h/960
+    cropped = img[0:min(int(45*vscale), 65), 0:w]
+    gray = cv.cvtColor(cropped, cv.COLOR_BGR2GRAY)
+    zoomed = pi.zoom_img(gray, 2, 2)
+
+    ret, thresh = cv.threshold(zoomed, 50, 255, cv.THRESH_BINARY_INV)
+
+    # TODO : reajustar estes valores
+    thresholds = [190, 200, 210, 165, 150, 130, 100, 85, 50, 25]
+    extracted_text = pi.try_multiple_thresholds_ocr(
+        zoomed, thresholds, pt.validate_hour_hh_mm)
+
+    return extracted_text
+
+
+def extract(img):
+    h, w, c = img.shape
+
+    phone_time = scan_for_current_time(img)
+    time_until_finish, did_egg_hatch = scan_for_time_until_finish(img)
+
+    return phone_time, time_until_finish, did_egg_hatch
+
+
+def scan_for_time_until_finish(img):
+    orange = [243, 121, 53]
+    time_until_finish = pi.section_by_color(img, orange,  [0.5, 0.65], [0.8, 0.99], 34, 0, [
+    ], regex=pt.validate_hour_hh_mm_ss)
+    did_egg_hatch = True
+
+    if not time_until_finish:
+        # print("Testing timer before hatch")
+        pink = [243, 136, 142]
+        time_until_finish = pi.section_by_color(img, pink,  [0.15, 0.2], [0.45, 0.85], 50, 0, [
+        ], pixels_quantity=150, regex=pt.validate_hour_hh_mm_ss)
+        did_egg_hatch = False
+
+    return time_until_finish, did_egg_hatch
+
+
+def extract_level(img):
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    template = cv.imread('images/raids/raid_icon.png', 0)
+    template = cv.resize(template, None, fx=0.25,
+                         fy=0.25, interpolation=cv.INTER_CUBIC)
+
+    loc = [[]]
+    max_level = 0
+    while(True):
+        level = 0
+        w, h = template.shape[::-1]
+
+        if w < 22 and h < 22:
+            break
+
+        # print(w, h)
+        # TODO: analizar os elemtnos q se sobrepoem
+
+        res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+        threshold = 0.65
+        loc = np.where(res >= threshold)
+
+        # print(loc)
+        x_dist = 0
+        for pt in zip(*loc[::-1]):
+            cv.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+            # cv.imshow('res.png', img_rgb)
+
+        # [187, 188, 221, 222, 256, 290, 324, 325])
+
+        x_points = loc[1]
+        x_points.sort()
+
+        # print("XPOINTS")
+        # print(x_points)
+
+        for (index, point) in enumerate(x_points[:-1]):
+            diff = abs(loc[1][index + 1] - point)
+            if diff > w/2:
+                # print("Prev {} - Next {}".format(point, loc[1][index+1]))
+                level += 1
+
+        max_level = max(level, max_level)
+
+        if max_level == 5:
+            break
+
+        if len(loc[0]) >= 1 and level == 0:
+            level = 1
+            # cv.imshow("output", img_rgb)
+            # cv.imshow("output2", template)
+            # cv.waitKey(0)
+
+        template = cv.resize(template, None, fx=0.95,
+                             fy=0.95, interpolation=cv.INTER_CUBIC)
+
+    # print("Level {}".format(max_level))
+
+    return max_level
+
+
+def extract_gym_name(img, coords):
+    w, h, c = img.shape
+    img = img[coords[0]-coords[2]:coords[0] +
+              coords[2], coords[1]+coords[2]:w]
+
+    img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    ret, img = cv.threshold(img,
+                            205, 255, cv.THRESH_BINARY_INV)
+
+    return pytesseract.image_to_string(
+        img, config='--psm 6')
--- ocr/utils/process_img.py
+++ ocr/utils/process_img.py
@@ -1,9 +1,16 @@
+
+import pytesseract
+import re
 import cv2 as cv
-import color as co
+import numpy as np
+from utils import color as co
+from utils import process_img as pi
+from utils import name_ratio as nr
+from utils import get_bosses as gb
 
 
 def read_image_pokemon(img_name):
-    img_path = "raids/pokemon/" + str(img_name) + ".jpg"
+    img_path = "images/raids/pokemon/" + str(img_name)
     img = cv.imread(img_path)
     return img
 
@@ -65,3 +72,203 @@ def remove_bottom_bar(img):
             img = img[0:h, 0:w]
 
     return img
+
+
+def zoom_img(img, fx, fy):
+    return cv.resize(img, None, fx=2, fy=2,
+                     interpolation=cv.INTER_CUBIC)
+
+
+def try_multiple_thresholds_ocr(gray_img, thresholds, regex=()):
+
+    for th in thresholds:
+        ret, thresh = cv.threshold(gray_img, th, 255, cv.THRESH_BINARY_INV)
+
+        text = ocr_numbers_single_row(thresh)
+
+        if not regex == ():
+            extracted_text = regex(text)
+
+        if extracted_text:
+            return extracted_text
+
+    return None
+
+
+def ocr_numbers_single_row(img):
+    return pytesseract.image_to_string(
+        img, config='-c tessedit_char_whitelist=1234567890:~-AMP --psm 7')
+
+
+def section_by_color(img, color_goal, start, end, block_height, threshold_type, thresholds, pixels_quantity=85, regex=()):
+    color_found_initial_coords = []
+    found_pixels = []
+    count_pixels = 0
+    h, w, c = img.shape
+
+    vscale = h/960
+    hscale = w/500
+
+    # print("Block heigh {}".format(block_height))
+
+    # y,x
+    init_x = int(w * start[1])
+    init_y = int(h * start[0])
+
+    x = init_x
+    y = init_y
+
+    max_x = int(w*end[1])
+    max_y = int(h*end[0])
+
+    while(True):
+
+        # Nothing relevant extracted
+        if x > max_x and y > max_y:
+            return None
+
+        if x < max_x:
+            x += 1
+        else:
+            x = init_x
+            if y > max_y:
+                break
+            y += 1
+
+        # WARNING img[y,x] returns BGR instead of RGB
+        if co.arbitrary_similarity(img[y, x][::-1], color_goal, 20):
+            count_pixels += 1
+            found_pixels.append([y, x])
+        else:
+            count_pixels = 0
+
+        if count_pixels > int(pixels_quantity * hscale):
+            cut_img = cut_block(img, found_pixels, int(block_height * vscale))
+            gray = cv.cvtColor(cut_img, cv.COLOR_BGR2GRAY)
+            zoomed = zoom_img(gray, 2, 2)
+            text = ''
+
+            if threshold_type == 0:
+                text = threshold_binary_inv(zoomed, 200, regex)
+            elif threshold_type == 1:
+                # thresholds = [180, 190, 200, 210, 215, 100, 125, 150, 175]
+                text = try_multiple_thresholds_ocr(zoomed, thresholds, regex)
+
+            if text:
+                return text
+
+            break
+
+
+def cut_block(img, found_pixels, block_height):
+    min_x = 9999
+    max_x = -1
+
+    for arr in found_pixels:
+        if arr[1] < min_x:
+            min_x = arr[1]
+            color_found_initial_coords = arr
+        if arr[1] > max_x:
+            max_x = arr[1]
+
+    box_width = max_x - min_x
+    sec_x = color_found_initial_coords[1]
+    sec_y = color_found_initial_coords[0]
+
+    # print("Encontrou um bloco de cor com inicio em x.{} y.{}".format(
+    #     sec_x, sec_y))
+
+    return img[sec_y:sec_y+block_height, sec_x:sec_x+box_width]
+
+
+def threshold_binary_inv(gray_img, thresh_val, regex=()):
+    ret, thresh = cv.threshold(
+        gray_img, thresh_val, 255, cv.THRESH_BINARY_INV)
+
+    # cv.imshow("img", thresh)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+
+    text = ocr_numbers_single_row(thresh)
+    extracted_text = regex(text)
+
+    if extracted_text:
+        return extracted_text
+
+    return None
+
+
+def detect_circles(img):
+
+    output = img.copy()
+    gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+    # detect circles in the image
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT,
+                              2, 150,  minRadius=25, maxRadius=100)
+
+    image_circle = None
+    # ensure at least some circles were found
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+
+        min_x = 9999
+        min_y = 9999
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            cv.circle(output, (x, y), r, (0, 255, 0), 4)
+            cv.rectangle(output, (x - 5, y - 5),
+                         (x + 5, y + 5), (0, 128, 255), -1)
+
+            if x < min_x and y < min_y:
+                min_x = x
+                min_y = y
+                image_circle = (min_y, min_x, r)
+
+        # show the output image
+        # cv.imshow("output", np.hstack([img, output]))
+        # cv.waitKey(0)
+
+    return image_circle
+
+
+def find_boss_name(img, raid_level):
+    # FIXME: remover barra superior, falham as imagens 3, 6 e 41.
+    new_img = pi.remove_bottom_bar(img)
+
+    processed_img = pi.crop_resize_img(new_img)
+
+    pokemon_name_img = pi.crop_pokemon_name(processed_img)
+
+    thresh = pi.get_threshold(pokemon_name_img, 245, True)
+
+    text_found = pytesseract.image_to_string(
+        thresh, config="--psm 8")
+
+    boss_name_found = re.sub('[^a-zA-Z]', '', text_found)
+
+    bosses_id = gb.current_boss_by_level(raid_level)
+
+    bosses = gb.current_boss_names_by_id(bosses_id)
+
+    max_ratio = 0
+
+    for boss_name in bosses:
+        ratio = nr.levenshtein_ratio_and_distance(
+            boss_name_found.lower(), boss_name.lower(), ratio_calc=True)
+
+        if ratio > max_ratio:
+            max_ratio = ratio
+            pokemon_name = boss_name
+
+    print('=' * 30)
+    print('Text found: {}'.format(text_found))
+    print('Text found with regex: {}'.format(boss_name_found))
+    print('Best match name: {}'.format(pokemon_name))
+    print('Best match ratio: {}'.format(max_ratio))
+    print('=' * 30)
+
+    return pokemon_name
